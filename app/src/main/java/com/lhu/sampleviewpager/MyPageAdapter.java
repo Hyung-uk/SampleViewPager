3a22bb28f5551cecc4336a80cdce6b797ab78eaf
hyung-uk <lhu114@nate.com>
2015-10-08 17:06:18 +0900

    Initial commit

---
+++ app/src/main/java/com/lhu/sampleviewpager/MyPageAdapter.java
@@ -0,0 +1,53 @@
+package com.lhu.sampleviewpager;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Tacademy on 2015-10-08.
+ */
+public class MyPageAdapter extends PagerAdapter {
+
+
+    List<View> scrappedView = new ArrayList<View>();
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view;
+
+        if (scrappedView.size() > 0) {
+            view = scrappedView.remove(0);
+        } else {
+            view = LayoutInflater.from(container.getContext()).inflate(R.layout.view_item, container, false);
+        }
+
+        TextView tv = (TextView) view.findViewById(R.id.textView);
+        tv.setText("number : " + position);
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        View view = (View) object;
+        container.removeView(view);
+        scrappedView.add(view);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
